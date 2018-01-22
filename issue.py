b590ad95f89a62efea0ec2f75c8fd63bd89fcf5a
Takanori Suzuki <takanory@beproud.jp>
2018-01-22 12:56:46 +0900

    INU-49 fix channel

--- issue.py
+++ issue.py
@@ -14,7 +14,7 @@ QUERY = '''project = {project} AND status in (Open, "In Progress", Reopened)
 PROJECTS = {
     'INU': [
         # (コンポーネント, チャンネル)
-        (('0.全体', '9.その他'), '#2017'),
+        (('0.全体', '9.その他'), '#2018'),
         ('1.事務局', '#t-jimukyoku'),
         ('2.会場', '#t-venue'),
         ('3.システム', '#t-system'),
